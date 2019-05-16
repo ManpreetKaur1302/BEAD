6c8155b1a069a8d06283db0bd32acd909b858d74
ManpreetKaur1302 <kaur6668@sheridancollege.ca>
2019-05-16 15:05:15 -0400

    Initial Commit

---
+++ src/bead/Bead.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bead;
+
+/**
+ *
+ * @author MANPREET KAUR
+ */
+public class Bead {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    private String color;
+    private char letter;
+
+    /**
+     * @return the color
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    /**
+     * @return the letter
+     */
+    public char getLetter() {
+        return letter;
+    }
+
+    /**
+     * @param letter the letter to set
+     */
+    public void setLetter(char letter) {
+        this.letter = letter;
+    }
+    
+}
